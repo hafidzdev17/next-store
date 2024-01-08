65d3f0dc5dab2ab8e7fa663cf7f14fa07c94146f
hafidzdev17 <do.crazy192@gmail.com>
2024-01-08 22:53:57 +0700

    feat: update configure firebase & signup function

--- src/lib/firebase/service.ts
+++ src/lib/firebase/service.ts
@@ -4,8 +4,12 @@ import {
   getDocs,
   getDoc,
   doc,
+  addDoc,
+  query,
+  where,
 } from "firebase/firestore";
 import app from "./init";
+import bcrypt from "bcrypt";
 
 const firestore = getFirestore(app);
 
@@ -22,4 +26,45 @@ export async function retrieveDataById(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(firestore, collectionName, id));
   const data = snapshot.data();
   return data;
+}
+
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    phone: string;
+    password: string;
+    role?: string;
+  },
+  callback: Function
+) {
+  const q = query(
+    collection(firestore, "users"),
+    where("email", "==", userData.email)
+  );
+
+  const snapshot = await getDocs(q);
+
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  if (data.length > 0) {
+    callback(false);
+  } else {
+    if (!userData.role) {
+      userData.role = "member";
+    }
+    userData.password = await bcrypt.hash(userData.password, 10);
+
+    await addDoc(collection(firestore, "users"), userData)
+      .then(() => {
+        callback(true);
+      })
+      .catch((err) => {
+        callback(false);
+        console.log(err);
+      });
+  }
 }
